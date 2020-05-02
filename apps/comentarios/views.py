5b35efa17bd2e1240a374abbd2823d41a1533889
Juanjo <josejuan250203@gmail.com>
2020-05-01 21:40:09 -0500

    HPTAAAAAAAAAA

---
+++ apps/comentarios/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# Create your views here.
+
+class Registro(TemplateView):
+    template_name = 'registro.html'
+
+class Login(TemplateView):
+    template_name = 'login.html'
+
+class Home(TemplateView):
+    template_name = "index.html"
